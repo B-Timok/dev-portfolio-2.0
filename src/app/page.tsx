56b35883528bcb008dedae9622ab7d263ff5b5fa
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2025-03-14 22:07:55 -0700

    Update page.tsx reorder sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
       <main>
         <HeroSection />
         <ProjectsSection />
-        <AboutSection />
         <ExperienceSection />
+        <AboutSection />
         <ContactSection />
       </main>
       <Footer />
